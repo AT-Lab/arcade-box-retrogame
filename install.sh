6b23a87337163ce3f2b6286216e247fdef072190
Davide <pasettodavide@gmail.com>
2018-04-19 12:21:30 +0200

    Script di installazione, da provare

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env/ sh
+
+echo "Script di installazione dei file necessari all'utilizzo delle GPIO come tastiera"
+echo "Creato dal team AT Lab"
+
+if [ "$(id -u)" -ne 0 ]
+then echo "Lanciare questo script come sudo ./$0"
+    exit
+else
+    if [ -e retrogame ]
+    then
+        echo "Copio retrogame in .bin..."
+        cp retrogame "/storage/retrogame"
+    else
+        echo "File retrograme non trovato"
+        exit
+    fi
+
+    if [ -e retrogame.cfg ]
+    then
+        echo "Copio retrogame.cfg in .bin..."
+        cp retrogame "/storage/retrogame.cfg"
+    else
+        echo "File retrograme.cfg non trovato"
+        exit
+    fi
+
+    if [ -e retrogame.service ]
+    then
+        echo "Copio retrogame.service in .bin..."
+        cp retrogame "/storage/.config/systemd/user/retrogame.service"
+    else
+        echo "File retrograme.service non trovato"
+        exit
+    fi
+
+    systemctl enable retrogame.service
+    systemctl start retrogame.sercice
+
+    printf "Finito, riavvia la macchina con il comando\nsudo systemctl reboot"
+
+fi
+
